08b7683c705bf405bda52b4cdef63129bb5157a4
tsinin <andrew.tsinin@mail.ru>
2018-11-20 02:15:47 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/binomialHeap.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binomialHeap.dir/main.cpp.obj"
+  "CMakeFiles/binomialHeap.dir/Tests/test.cpp.obj"
+  "binomialHeap.pdb"
+  "binomialHeap.exe"
+  "binomialHeap.exe.manifest"
+  "libbinomialHeap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binomialHeap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
