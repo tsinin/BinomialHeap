b13a00fc1a1c610c992ff591d745ee17515f31cb
tsinin <andrew.tsinin@mail.ru>
2018-11-20 02:10:59 +0300

    initial commit

---
+++ Tests/binomialHeap.h
@@ -0,0 +1,242 @@
+//
+// Created by Tsinin on 09.11.2018.
+//
+#include "Vector.h"
+#include <memory>
+
+template <typename Key>
+class binomialHeap {
+public:
+    class Pointer;
+    struct node {
+        node *firstChild, *lastChild, *parent, *next, *prev;
+        Pointer* backPtr;
+        int degree = 0;
+        bool smallest = false;
+        Key data;
+        node(Key newData) {
+            data = newData;
+            backPtr = nullptr;
+            firstChild = lastChild = parent = next = prev = nullptr;
+        };
+        node() {
+            backPtr = nullptr;
+            firstChild = lastChild = parent = next = prev = nullptr;
+        };
+    };
+    class Pointer {
+    public:
+        ~Pointer() = default;
+    private:
+        friend class binomialHeap<Key>;
+        Pointer(node* new_ptr): element(new_ptr) {}
+        node* element;
+    };
+    bool is_empty() const {
+        return (firstNode == nullptr);
+    }
+    void merge(binomialHeap &otherHeap) {
+        node *firstPointer = firstNode, *secondPointer = otherHeap.firstNode;
+        Vector<node*> queue;
+        while(firstPointer && secondPointer) {
+            if(firstPointer->degree < secondPointer->degree) {
+                queue.push_back(firstPointer);
+                if(!firstPointer->next){
+                    firstPointer->prev = firstPointer->parent = nullptr;
+                    firstPointer = firstPointer->next;
+                }
+                else {
+                    firstPointer->prev = firstPointer->parent = nullptr;
+                    firstPointer = firstPointer->next;
+                    firstPointer->prev->next = nullptr;
+                }
+            } else {
+                queue.push_back(secondPointer);
+                if(!secondPointer->next){
+                    secondPointer->prev = secondPointer->parent = nullptr;
+                    secondPointer = secondPointer->next;
+                }
+                else {
+                    secondPointer->prev = secondPointer->parent = nullptr;
+                    secondPointer = secondPointer->next;
+                    secondPointer->prev->next = nullptr;
+                }
+            }
+        }
+        while(firstPointer) {
+            queue.push_back(firstPointer);
+            if(!firstPointer->next){
+                firstPointer->prev = firstPointer->parent = nullptr;
+                firstPointer = firstPointer->next;
+            }
+            else {
+                firstPointer->prev = firstPointer->parent = nullptr;
+                firstPointer = firstPointer->next;
+                firstPointer->prev->next = nullptr;
+            }
+        }
+        while(secondPointer) {
+            queue.push_back(secondPointer);
+            if(!secondPointer->next){
+                secondPointer->prev = secondPointer->parent = nullptr;
+                secondPointer = secondPointer->next;
+            }
+            else {
+                secondPointer->prev = secondPointer->parent = nullptr;
+                secondPointer = secondPointer->next;
+                secondPointer->prev->next = nullptr;
+            }
+        }
+        int current = 0;
+        node* temp = new node();
+        node* begin = temp;
+        while(current < queue.size()) {
+            while(current < queue.size() - 2 && queue[current]->degree == queue[current + 2]->degree) {
+                queue[current + 2] = merge(queue[current], queue[current + 2]);
+                temp->next = queue[current + 1];
+                temp->next->prev = temp;
+                temp = temp->next;
+                current += 2;
+            }
+            bool goAgain = false;
+            while(current < queue.size() - 1 && queue[current]->degree == queue[current + 1]->degree) {
+                if(current < queue.size() - 2 && queue[current]->degree == queue[current + 2]->degree) {
+                    goAgain = true;
+                    break;
+                }
+                queue[current + 1] = merge(queue[current], queue[current + 1]);
+                current++;
+            }
+            if(goAgain)
+                continue;
+            temp->next = queue[current];
+            temp->next->prev = temp;
+            temp = temp->next;
+            current++;
+        }
+        firstNode = begin->next;
+        if(firstNode)
+            firstNode->prev = nullptr;
+    }
+    Pointer* insert(Key x) {
+        binomialHeap<Key> newHeap;
+        newHeap.firstNode = new node(x);
+        Pointer* pointer = new Pointer(newHeap.firstNode);
+        newHeap.firstNode->backPtr = pointer;
+        this->merge(newHeap);
+        return pointer;
+    }
+    Key get_min()  {
+        try {
+            if(this->is_empty())
+                throw std::logic_error("Heap is empty.");
+        }
+        catch(std::logic_error& error) {
+            std::cerr << error.what() << std::endl;
+            exit(1);
+        }
+        node* cur = firstNode;
+        Key minElement = cur->data;
+        while(cur) {
+            if(cur->data < minElement)
+                minElement = cur->data;
+            cur = cur->next;
+        }
+        return minElement;
+    }
+    Key extract_min() {
+        try {
+            if(this->is_empty())
+                throw std::logic_error("Heap is empty.");
+        }
+        catch(std::logic_error& error) {
+            std::cerr << error.what() << std::endl;
+            exit(1);
+        }
+        node* cur = firstNode;
+        node* minElement = cur;
+        while(cur) {
+            if(cur->data < minElement->data || cur->smallest)
+                minElement = cur;
+            cur = cur->next;
+        }
+        Key ans = minElement->data;
+
+        binomialHeap<Key> secondHeap;
+        secondHeap.firstNode = minElement->firstChild;
+        if(minElement->prev && minElement->next) {
+            minElement->prev->next = minElement->next;
+            minElement->next->prev = minElement->prev;
+        } else if(minElement->prev) {
+            minElement->prev->next = nullptr;
+        } else if(minElement->next){
+            firstNode = minElement->next;
+            minElement->next->prev = nullptr;
+        } else
+            firstNode = nullptr;
+
+        delete minElement->backPtr;
+        delete minElement;
+        this->merge(secondHeap);
+        return ans;
+    }
+    void erase(Pointer* pointer) {
+        pointer->element->smallest = true;
+        decreaseKey(pointer, pointer->element->data);
+        delete pointer;
+        this->extract_min();
+    }
+    void change(Pointer* pointer, Key newKey) {
+        if(newKey > pointer->element->data) {
+            this->erase(pointer);
+            pointer = insert(newKey);
+        } else {
+            decreaseKey(pointer, newKey);
+        }
+    }
+    binomialHeap() {};
+    ~binomialHeap() {};
+private:
+    node* firstNode = nullptr;
+    node* merge(node* first, node* second) {
+        if(first->data >= second->data) {
+            node* temp = first;
+            first = second;
+            second = temp;
+        }
+        if(!first->lastChild) {
+            first->lastChild = first->firstChild = second;
+            second->parent = first;
+            second->next = second->prev = nullptr;
+            first->degree++;
+            return first;
+        }
+        first->lastChild->next = second;
+        second->next = nullptr;
+        second->prev = first->lastChild;
+        first->lastChild = first->lastChild->next;
+        second->parent = first;
+        first->degree++;
+        return first;
+    }
+    void swapWithParent(node*& ptr) {
+        Key tempForData = ptr->data;
+        ptr->data = ptr->parent->data;
+        ptr->parent->data = tempForData;
+
+        Pointer* tempForBackPtr = ptr->backPtr;
+        ptr->backPtr = ptr->parent->backPtr;
+        ptr->parent->backPtr = tempForBackPtr;
+
+        ptr->backPtr->element = ptr->parent;
+        ptr->parent->backPtr->element = ptr;
+    }
+    void decreaseKey(Pointer* pointer, int newKey) {
+        node* currentNode = pointer->element;
+        currentNode->data = newKey;
+        while(currentNode->parent && (currentNode->smallest || currentNode->data < currentNode->parent->data)) {
+            swapWithParent(currentNode);
+            currentNode = currentNode->parent;
+        }
+    }
+};
---
+++ Tests/test.cpp
@@ -0,0 +1,188 @@
+//
+// Created by Tsinin on 09.11.2018.
+//
+#include <ctime>
+#include <set>
+#include <vector>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "binomialHeap.h"
+
+using testing::Eq;
+
+namespace {
+    class testFixture : public testing::Test {
+    public:
+        binomialHeap<int> heap;
+        Vector<int> testVector;
+        std::multiset<int> testSet;
+        testFixture() {
+            binomialHeap<int> heap; // could make here a make method
+
+        }
+    };
+}
+TEST_F(testFixture, isEmptyTest1) {
+    ASSERT_TRUE(heap.is_empty());
+}
+
+TEST_F(testFixture, isEmptyTest2) {
+    int t = rand();
+    heap.insert(t);
+    ASSERT_FALSE(heap.is_empty());
+}
+TEST_F(testFixture, exceptionTest1) {
+    ASSERT_EXIT(heap.get_min(), ::testing::ExitedWithCode(1), "Heap is empty.");
+}
+
+TEST_F(testFixture, exceptionTest2) {
+    ASSERT_EXIT(heap.extract_min(), ::testing::ExitedWithCode(1), "Heap is empty.");
+}
+
+TEST_F(testFixture, smallTest1) {
+    heap.insert(1);
+    ASSERT_EQ(heap.get_min(), 1);
+    heap.insert(2);
+    ASSERT_EQ(heap.get_min(), 1);
+    heap.insert(3);
+    ASSERT_EQ(heap.get_min(), 1);
+    ASSERT_EQ(heap.extract_min(), 1);
+    ASSERT_EQ(heap.extract_min(), 2);
+    ASSERT_EQ(heap.extract_min(), 3);
+}
+
+TEST_F(testFixture, smallTest2) {
+    binomialHeap<int>::Pointer* p1 = heap.insert(1),
+    *p2 = heap.insert(2),
+    *p3 = heap.insert(3);
+    heap.erase(p1);
+    ASSERT_EQ(heap.get_min(), 2);
+    heap.erase(p3);
+    ASSERT_EQ(heap.get_min(), 2);
+    p3 = heap.insert(3);
+    heap.extract_min();
+    ASSERT_EQ(heap.get_min(), 3);
+}
+
+TEST_F(testFixture, smallTest3) {
+    binomialHeap<int>::Pointer* p1 = heap.insert(1),
+    *p2 = heap.insert(2);
+    heap.change(p1, 3);
+    ASSERT_EQ(heap.get_min(), 2);
+    heap.change(p2, 4);
+    ASSERT_EQ(heap.get_min(), 3);
+}
+
+TEST_F(testFixture, smallTest4) {
+    binomialHeap<int>::Pointer* p1 = heap.insert(1),
+    *p2 = heap.insert(2),
+    *p3 = heap.insert(3);
+    heap.change(p1, 0);
+    ASSERT_EQ(heap.get_min(), 0);
+    heap.change(p1, 1);
+    ASSERT_EQ(heap.get_min(), 1);
+    heap.change(p2, 0);
+    ASSERT_EQ(heap.get_min(), 0);
+    heap.change(p2, 4);
+    ASSERT_EQ(heap.extract_min(), 1);
+    ASSERT_EQ(heap.extract_min(), 3);
+    ASSERT_EQ(heap.extract_min(), 4);
+}
+
+TEST_F(testFixture, simpleTest1) {
+    for(int i = 400000; i >= 0; --i)
+        heap.insert(i);
+    for(int i = 0; i < 400000; ++i)
+        ASSERT_EQ(i, heap.extract_min());
+}
+
+TEST_F(testFixture, simpleTest2) {
+    for(int i = 0; i < 400000; ++i)
+        heap.insert(i);
+    for(int i = 0; i < 400000; ++i)
+        ASSERT_EQ(i, heap.extract_min());
+}
+
+TEST_F(testFixture, randomSmallTest) {
+    for(int i = 0; i < 40000; ++i) {
+        int operation = rand() % 2;
+        if(operation && !testVector.size() || testVector.size() < 5000)
+            operation = 0;
+        if(operation) {
+            int minElementPtr = 0;
+            for(int i = 1; i < testVector.size(); ++i)
+                if(testVector[i] < testVector[minElementPtr])
+                    minElementPtr = i;
+            int minElement = testVector[minElementPtr];
+            ASSERT_EQ(heap.get_min(), minElement);
+            for(int i = minElementPtr; i < testVector.size() - 1; ++i)
+                testVector[i] = testVector[i + 1];
+            testVector.pop_back();
+            heap.extract_min();
+        } else {
+            int x = rand();
+            testVector.push_back(x);
+            heap.insert(x);
+        }
+    }
+}
+
+TEST_F(testFixture, randomBigTest1) {
+    time_t start = clock(), end;
+
+    for(int i = 0; i < 1000000; ++i) {
+        int operation = rand() % 2;
+        if(operation && !testSet.size())
+            operation = 0;
+        if(operation) {
+            ASSERT_EQ(heap.extract_min(), *(testSet.begin()));
+            testSet.erase(testSet.begin());
+        } else {
+            int x = rand();
+            testSet.insert(x);
+            heap.insert(x);
+        }
+    }
+    end = clock();
+    ASSERT_GE(2, ((double)end - start) / (double)(CLOCKS_PER_SEC));
+}
+
+TEST_F(testFixture, randomBigTes2) {
+    time_t start = clock(), end;
+
+    for(int i = 0; i < 1000000; ++i) {
+        int operation = rand() % 2;
+        if(operation && !testSet.size() || testSet.size() < 10000)
+            operation = 0;
+        if(operation) {
+            ASSERT_EQ(heap.extract_min(), *(testSet.begin()));
+            testSet.erase(testSet.begin());
+        } else {
+            int x = rand();
+            testSet.insert(x);
+            heap.insert(x);
+        }
+    }
+    end = clock();
+    ASSERT_GE(2, ((double)end - start) / (double)(CLOCKS_PER_SEC));
+}
+
+TEST_F(testFixture, randomBigTest3) {
+    time_t start = clock(), end;
+
+    for(int i = 0; i < 1000000; ++i) {
+        int operation = rand() % 2;
+        if(operation  && !testSet.size() || testSet.size() < 70000)
+            operation = 0;
+        if(operation) {
+            ASSERT_EQ(heap.extract_min(), *(testSet.begin()));
+            testSet.erase(testSet.begin());
+        } else {
+            int x = rand();
+            testSet.insert(x);
+            heap.insert(x);
+        }
+    }
+    end = clock();
+    ASSERT_GE(3, ((double)end - start) / (double)(CLOCKS_PER_SEC));
+}
